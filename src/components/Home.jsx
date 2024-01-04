8a7053583b40f615cd086565886d179f83c8195c
iyanuloluwa-Miracle <iyanudina@gmail.com>
2024-01-04 22:07:51 +0100

    feat: Home Sectionadded

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,8 +3,8 @@ import { MdOutlineKeyboardArrowRight } from "react-icons/md";
 import HeroImage from "../assets/heroImage.png";
 const Home = () => {
   return (
-    <section>
-      <div>
+    <section name="home" className="flex h-screen w-full bg-gradient-to-b from-black to-gray-800">
+      <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
         <div>
           <h2>
             I'm a Software Engineer
@@ -18,8 +18,16 @@ const Home = () => {
           <div>
             <button>
               Portfolio
-              <MdOutlineKeyboardArrowRight />
+              <span>
+                <MdOutlineKeyboardArrowRight />
+
+              </span>
+              
             </button>
+          </div>
+
+          <div>
+            <img src={HeroImage} alt="HeroImage"/>
           </div>
         </div>
       </div>
